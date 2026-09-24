c366c0a23214c369481e663c1b99a0d3dc2d06a9
Mohamed Hedi MEHDOUI <mohamedhedi.mehdoui@esprit.tn>
2026-09-24 14:32:17 +0100

    Ajout des autres entités

---
+++ autoloc-api/src/main/java/tn/esprit/autoloc/domain/Equipement.java
@@ -0,0 +1,20 @@
+package tn.esprit.autoloc.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "equipement")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Equipement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEquipement;
+
+    @Column(nullable = false, length = 100)
+    private String libelle;
+}
